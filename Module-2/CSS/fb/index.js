905069d823f6abb38728f07abf708f432c29e859
Cass81097 <truonghoanganh.810@gmail.com>
2023-06-13 14:55:24 +0700

    push fb

---
+++ Module-2/CSS/fb/index.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip({'delay': { show: 5000, hide: 3000 }
+    });   
+});
