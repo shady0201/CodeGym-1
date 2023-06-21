505d09c449c857109cbe7a4c40f88f3654e10ce1
hoangvu0201 <hoangvushady0201@gmail.com>
2023-06-20 18:44:05 -0700

    fix style

--- Module-3/nodeJS/bt2/app.js
+++ Module-3/nodeJS/bt2/app.js
@@ -1,43 +1,68 @@
 import http from 'http';
 import fs from 'fs'; 
 import qs from 'qs';
+import url from 'url';
 import { StudentServices } from './services/studentServices.js';
 
-let studentServices = new StudentServices()
+let studentServices = new StudentServices();
+
+const typeFile = {
+    'jpg': 'images/jpg',
+    'png': 'images/png',
+    'js': 'text/javascript',
+    'css': 'text/css',
+    'svg': 'image/svg+xml',
+    'ttf': 'font/tff',
+    'woff': 'font/woff',
+    'woff2': 'font/woff',
+    'eot': 'application/vnd.ms-fontobject'
+}
 
 const server = http.createServer((req, res) => {
-    if (req.method === 'GET') {
-        showList(req, res);
 
-    } else {
-        let data = '';
-        req.on('data', (dataRaw) => {
-            data += dataRaw;
-        });
-        
-        req.on('end', () => {
-            const parsedData = qs.parse(data);
-            if (parsedData.search) {
-                const searchResults = studentServices.findStudentByName(parsedData.search);
-                renderHtml(res, searchResults);
-            } 
-
-            else if (parsedData.deleteId) {
-                const deleteResults = studentServices.deleteStudent(parsedData.deleteId);
-                renderHtml(res, deleteResults);
-            } 
-
-            else if (parsedData.id && parsedData.name && parsedData.score) {
-                const newData = { id: parsedData.id, name: parsedData.name, score: parsedData.score };
-                studentServices.addStudent(newData);
-                showList(req, res);
-            } 
-
-            else {
-                res.end('Loi!');
-            }
-        });
+    // file handle
+    const pathName = url.parse(req.url, true).pathname;
+    const checkPath = pathName.match(/\.js|\.css|\.png|\.jpg|\.ttf|\.woff|\.woff2|\.eot/);
+    if (checkPath) {
+        const contentType = typeFile[checkPath[0].toString().split('.')[1]];
+        res.writeHead(200, {'Content-Type': contentType});
+        fs.createReadStream(process.cwd() + req.url).pipe(res);
+    }else{
+        if (req.method === 'GET') {
+            showList(req, res);
+    
+        } else {
+            let data = '';
+            req.on('data', (dataRaw) => {
+                data += dataRaw;
+            });
+            
+            req.on('end', () => {
+                const parsedData = qs.parse(data);
+                if (parsedData.search) {
+                    const searchResults = studentServices.findStudentByName(parsedData.search);
+                    renderHtml(res, searchResults);
+                } 
+    
+                else if (parsedData.deleteId) {
+                    const deleteResults = studentServices.deleteStudent(parsedData.deleteId);
+                    renderHtml(res, deleteResults);
+                } 
+    
+                else if (parsedData.id && parsedData.name && parsedData.score) {
+                    const newData = { id: parsedData.id, name: parsedData.name, score: parsedData.score };
+                    studentServices.addStudent(newData);
+                    showList(req, res);
+                } 
+    
+                else {
+                    res.end('Loi!');
+                }
+            });
+        }
     }
+
+    
 });
 
 function showList(req, res) {
@@ -46,7 +71,7 @@ function showList(req, res) {
 }
 
 function renderHtml(res, items) {
-    fs.readFile('./views/listStudent.html', 'utf-8', (err, html) => {
+    fs.readFile('./bt2/views/listStudent.html', 'utf-8', (err, html) => {
         let itemsHtml = '';
         for (const item of items) {
             itemsHtml += `<h2>${item.id}. ${item.name} - ${item.score}</h2>`
